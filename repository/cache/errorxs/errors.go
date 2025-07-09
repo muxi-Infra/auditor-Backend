73a3efc3daa3635c2ff30b17e5b3d3d7d0d8e5b5
cqhasy <3181940782@qq.com>
2025-07-09 16:16:57 +0800

    fix (分离tag与select逻辑)

---
+++ repository/cache/errorxs/errors.go
@@ -0,0 +1,26 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type CacheNotFoundError struct {
+	Err error
+}
+
+func (e CacheNotFoundError) Error() string {
+	return fmt.Sprintf("cache not found: %v", e.Err)
+}
+
+func (e CacheNotFoundError) Unwrap() error {
+	return e.Err
+}
+
+func ToCacheNotFoundError(err error) error {
+	return CacheNotFoundError{Err: err}
+}
+func IsCacheNotFoundError(err error) bool {
+	var target CacheNotFoundError
+	return errors.As(err, &target)
+}
